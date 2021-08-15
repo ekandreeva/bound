f26fd8c4ee703f7bcc0e7b6f6edf01eef327b97b
ekandreeva <e.andreeva0304@gmail.com>
2021-08-15 20:13:57 +0300

    add models

--- bound_api/admin.py
+++ bound_api/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Order
 from .models import User
 from .models import Address
+from .models import PaymentMethod
+from .models import Payment
 from customer_app.models import Customer
 from driver_app.models import Driver
 
@@ -12,3 +14,5 @@ admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Address)
+admin.site.register(PaymentMethod)
+admin.site.register(Payment)
--- bound_api/models/__init__.py
+++ bound_api/models/__init__.py
@@ -2,3 +2,5 @@ from .user import User
 from .order import Order
 from .address import Address
 from .orders_users import OrdersUsers
+from .payment_method import PaymentMethod
+from .payment import Payment
